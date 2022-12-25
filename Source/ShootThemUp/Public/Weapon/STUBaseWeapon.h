a9be410d7b39221ad0a45473412a282d7709fef0
Artur Dzhus <asasin.lol10@gmail.com>
2022-12-25 14:01:11 +0200

    [REFACTORING] Added helper functions for MakeShot()

--- Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
+++ Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
@@ -30,4 +30,14 @@ protected:
     virtual void BeginPlay() override;
 
     void MakeShot();
+
+    APlayerController* GetPlayerController() const;
+
+    bool GetPlayerViewPoint(FVector& ViewLocation, FRotator& ViewRotation) const;
+
+    FVector GetMuzzleWorldLocation() const;
+
+    bool GetTraceData(FVector& TraceStart, FVector& TraceEnd) const;
+
+    void MakeHit(FHitResult& HitResult, const FVector& TraceStart, const FVector TraceEnd);
 };
