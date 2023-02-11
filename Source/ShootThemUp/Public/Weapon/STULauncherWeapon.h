bbd1caa12c19a48f5e71e0ec562abb3597511b79
Artur Dzhus <asasin.lol10@gmail.com>
2023-02-11 15:47:09 +0200

    Added spawn rockets

--- Source/ShootThemUp/Public/Weapon/STULauncherWeapon.h
+++ Source/ShootThemUp/Public/Weapon/STULauncherWeapon.h
@@ -6,12 +6,20 @@
 #include "Weapon/STUBaseWeapon.h"
 #include "STULauncherWeapon.generated.h"
 
-/**
- * 
- */
+
+class ASTUProjectile;
+
 UCLASS()
 class SHOOTTHEMUP_API ASTULauncherWeapon : public ASTUBaseWeapon
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    virtual void StartFire() override;
+
+protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapom")
+    TSubclassOf<ASTUProjectile> ProjectileClass;
+
+    virtual void MakeShot() override;
 };
