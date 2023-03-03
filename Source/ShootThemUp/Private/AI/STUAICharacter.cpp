2170bb5cd00acece154206da754ff5cabd0a3392
Artur Dzhus <asasin.lol10@gmail.com>
2023-03-03 15:58:02 +0200

    [Fix] AI Rotation seamless

--- Source/ShootThemUp/Private/AI/STUAICharacter.cpp
+++ Source/ShootThemUp/Private/AI/STUAICharacter.cpp
@@ -1,11 +1,18 @@
 // Shoot them up game
 
-
 #include "AI/STUAICharacter.h"
 #include "AI/STUAIController.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
-ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit) : Super(ObjInit) 
+ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit) : Super(ObjInit)
 {
     AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
     AIControllerClass = ASTUAIController::StaticClass();
+
+    bUseControllerRotationYaw = false;
+    if (GetCharacterMovement())
+    {
+        GetCharacterMovement()->bUseControllerDesiredRotation = true;
+        GetCharacterMovement()->RotationRate = FRotator(0.0f, 200.0f, 0.0f);
+    }
 }
--- Source/ShootThemUp/Private/AI/STUAIController.cpp
+++ Source/ShootThemUp/Private/AI/STUAIController.cpp
@@ -1,5 +1,15 @@
 // Shoot them up game
 
-
 #include "AI/STUAIController.h"
+#include "AI/STUAICharacter.h"
+void ASTUAIController::OnPossess(APawn* InPawn)
+{
+    Super::OnPossess(InPawn);
 
+    const auto STUCharacter = Cast<ASTUAICharacter>(InPawn);
+
+    if (STUCharacter)
+    {
+        RunBehaviorTree(STUCharacter->BehaviorTreeAsset);
+    }
+}
