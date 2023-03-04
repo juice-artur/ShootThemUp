0f4b4b57f34ad548b4cd05b0b657d3aaa06fdd68
Artur Dzhus <asasin.lol10@gmail.com>
2023-03-04 21:07:01 +0200

    Added change weapon service

---
+++ Source/ShootThemUp/Private/AI/Services/STUChangeWeaponService.cpp
@@ -0,0 +1,27 @@
+// Shoot them up game
+
+#include "STUChangeWeaponService.h"
+#include "Components/STUWeaponComponent.h"
+#include "AIController.h"
+#include "STUUtils.h"
+
+USTUChangeWeaponService::USTUChangeWeaponService()
+{
+    NodeName = "Change Weapon";
+}
+
+void USTUChangeWeaponService::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+    const auto Controller = OwnerComp.GetAIOwner();
+
+    if (Controller)
+    {
+        const auto WeaponComponent = STUUtils::GetSTUPlayerComponent<USTUWeaponComponent>(Controller->GetPawn());
+        if (WeaponComponent && Probability > 0 && FMath::FRand() <= Probability)
+        {
+            WeaponComponent->NextWeapon();
+        }
+    }
+
+    Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+}
