2ac86205a869d3fc2e57e5120e987df247c0c165
Artur Dzhus <asasin.lol10@gmail.com>
2023-03-04 10:54:10 +0200

    Added behavior for enemy detection

--- Source/ShootThemUp/Private/AI/STUAIController.cpp
+++ Source/ShootThemUp/Private/AI/STUAIController.cpp
@@ -3,6 +3,7 @@
 #include "AI/STUAIController.h"
 #include "AI/STUAICharacter.h"
 #include "Components/STUAIPerceptionComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 ASTUAIController::ASTUAIController()
 {
@@ -25,6 +26,16 @@ void ASTUAIController::OnPossess(APawn* InPawn)
 void ASTUAIController::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
-    const auto AimActor = STUAIPerceptionComponent->GetClosestEnemy();
+    const auto AimActor = GetFocusOnActor();
     SetFocus(AimActor);
+}
+
+AActor* ASTUAIController::GetFocusOnActor() const
+{
+    if (!GetBlackboardComponent())
+    {
+        return nullptr;
+    }
+
+    return Cast<AActor>(GetBlackboardComponent()->GetValueAsObject(FocusOnKeyName));
 }
