1d89253bbbca4cb54f706e0e56dff988630ff15e
Artur Dzhus <asasin.lol10@gmail.com>
2023-03-04 10:06:27 +0200

    Base setup AIPerceptionComponent

--- Source/ShootThemUp/Private/AI/STUAIController.cpp
+++ Source/ShootThemUp/Private/AI/STUAIController.cpp
@@ -2,6 +2,14 @@
 
 #include "AI/STUAIController.h"
 #include "AI/STUAICharacter.h"
+#include "Components/STUAIPerceptionComponent.h"
+
+ASTUAIController::ASTUAIController()
+{
+    STUAIPerceptionComponent = CreateDefaultSubobject<USTUAIPerceptionComponent>("STUAIPerceptionComponent");
+    SetPerceptionComponent(*STUAIPerceptionComponent);
+}
+
 void ASTUAIController::OnPossess(APawn* InPawn)
 {
     Super::OnPossess(InPawn);
@@ -12,4 +20,11 @@ void ASTUAIController::OnPossess(APawn* InPawn)
     {
         RunBehaviorTree(STUCharacter->BehaviorTreeAsset);
     }
+}
+
+void ASTUAIController::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+    const auto AimActor = STUAIPerceptionComponent->GetClosestEnemy();
+    SetFocus(AimActor);
 }
---
+++ Source/ShootThemUp/Private/Components/STUAIPerceptionComponent.cpp
@@ -0,0 +1,53 @@
+// Shoot them up game
+
+#include "Components/STUAIPerceptionComponent.h"
+#include "AIController.h"
+#include "STUUtils.h"
+#include "STUHealthComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+
+AActor* USTUAIPerceptionComponent::GetClosestEnemy() const
+{
+    TArray<AActor*> PersiveActors;
+    GetCurrentlyPerceivedActors(UAISense_Sight::StaticClass(), PersiveActors);
+
+    if (PersiveActors.Num() == 0)
+    {
+        return nullptr;
+    }
+
+    const auto Controller = Cast<AAIController>(GetOwner());
+
+    if (!Controller)
+    {
+        return nullptr;
+    }
+
+    const auto Pawn = Controller->GetPawn();
+
+    if (!Pawn)
+    {
+        return nullptr;
+    }
+
+    float BestDistance = MAX_FLT;
+    AActor* BestPawn = nullptr;
+
+    for (const auto PersiveActor : PersiveActors)
+    {
+        const auto HealthComponent = STUUtils::GetSTUPlayerComponent<USTUHealthComponent>(PersiveActor);
+
+        if (HealthComponent && !HealthComponent->IsDead())
+        {
+            const auto CurrentDistance = (PersiveActor->GetActorLocation() - Pawn->GetActorLocation()).Size();
+
+            if (CurrentDistance < BestDistance)
+            {
+                BestDistance = CurrentDistance;
+                BestPawn = PersiveActor;
+            }
+        }
+    }
+
+    return BestPawn;
+}
