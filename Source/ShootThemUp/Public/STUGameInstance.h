78dd8b6a04603149bb290224c3889c7e21f28d5a
Artur Dzhus <asasin.lol10@gmail.com>
2023-04-08 11:48:42 +0300

    Added Game Instance class

---
+++ Source/ShootThemUp/Public/STUGameInstance.h
@@ -0,0 +1,17 @@
+// Shoot them up game
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "STUGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API USTUGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
