82c83e66c62c9771e0694336d0f4ca0557805c3a
Artur Dzhus <asasin.lol10@gmail.com>
2023-04-08 15:24:12 +0300

    Added quit fame button

--- Source/ShootThemUp/Private/Menu/UI/STUMenuWidget.cpp
+++ Source/ShootThemUp/Private/Menu/UI/STUMenuWidget.cpp
@@ -4,6 +4,7 @@
 #include "Components/Button.h"
 #include "Kismet/GameplayStatics.h"
 #include "STUGameInstance.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogSTUMenuWidget, All, All);
 
@@ -12,6 +13,11 @@ void USTUMenuWidget::NativeOnInitialized()
     if (StartGameButton)
     {
         StartGameButton->OnClicked.AddDynamic(this, &USTUMenuWidget::OnStartGame);
+    }
+
+    if (QuitGameButton)
+    {
+        QuitGameButton->OnClicked.AddDynamic(this, &USTUMenuWidget::OnQuitGame);
     }
 }
 
@@ -35,4 +41,9 @@ void USTUMenuWidget::OnStartGame()
     }
 
     UGameplayStatics::OpenLevel(this, STUGameInstance->GetStartupLevelName());
+}
+
+void USTUMenuWidget::OnQuitGame()
+{
+    UKismetSystemLibrary::QuitGame(this, GetOwningPlayer(), EQuitPreference::Quit, true);
 }
